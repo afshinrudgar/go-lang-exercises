6c2e0e604cd9e71f54975087d258be5e260b3ac4
Afshin Rudgar <afshin.rudgar@eskano.com>
2017-12-22 13:27:13 +0330

    minor improvments in palindrom products solution

--- palindrome-products/palindrome_products.go
+++ palindrome-products/palindrome_products.go
@@ -1,84 +1,82 @@
 package palindrome
 
 import (
-	"strconv"
-	"errors"
-	"math"
+  "strconv"
+  "errors"
 )
 
 type Product struct {
-	Product        int      // palindromic, of course
-	Factorizations [][2]int //list of all possible two-factor factorizations of Product, within given limits, in order
+  Product        int      // palindromic, of course
+  Factorizations [][2]int //list of all possible two-factor factorizations of Product, within given limits, in order
 }
 
 func reversed(s string) string {
-	r := ""
-	for i := len(s) - 1; i >= 0; i-- {
-		r += string(s[i])
-	}
-	return r
+  r := ""
+  for i := len(s) - 1; i >= 0; i-- {
+    r += string(s[i])
+  }
+  return r
 }
 
 func isPalindrome(n int) bool {
-	s := strconv.Itoa(n)
-	return s == reversed(s)
+  s := strconv.Itoa(n)
+  return s == reversed(s)
 }
 
 func getFactorizations(n, fmin, fmax int) [][2]int {
-	var d, frac float64
-	fn := float64(n)
-	ffmin := float64(fmin)
-	ffmax := float64(fmax)
-	facts := make([][2]int, 0)
-	for i := ffmin; i < ffmax; i++ {
-		d, frac = math.Modf(fn / i)
-		if d < i {
-			return facts
-		}
-		if frac == 0 && d >= ffmin && d <= ffmax {
-			facts = append(facts, [2]int{int(i), int(d)})
-		}
-	}
-	return facts
+  facts := make([][2]int, 0)
+  for i := fmin; i < fmax; i++ {
+    if n % i != 0 {
+      continue
+    }
+    d := n / i
+    if d < i {
+      return facts
+    }
+    if d <= fmax && d >= fmin {
+      facts = append(facts, [2]int{i, d})
+    }
+  }
+  return facts
 }
 
 func Products(fmin, fmax int) (Product, Product, error) {
-	if fmin > fmax {
-		return Product{}, Product{}, errors.New("fmin > fmax...")
-	}
+  if fmin > fmax {
+    return Product{}, Product{}, errors.New("fmin > fmax...")
+  }
 
-	var mn, mx int
-	var mxProduct, mnProduct Product
-	ffmin := fmin * fmin
-	ffmax := fmax * fmax
+  var mn, mx int
+  var mxProduct, mnProduct Product
+  ffmin := fmin * fmin
+  ffmax := fmax * fmax
 
-	for mn = ffmin; mn < ffmax; mn++ {
-		if !isPalindrome(mn) {
-			continue
-		}
-		mnProduct = Product{Product: mn, Factorizations: getFactorizations(mn, fmin, fmax)}
-		if len(mnProduct.Factorizations) > 0 {
-			break
-		}
-	}
+  for mn = ffmin; mn < ffmax; mn++ {
+    if !isPalindrome(mn) {
+      continue
+    }
+    mnProduct = Product{Product: mn, Factorizations: getFactorizations(mn, fmin, fmax)}
+    if len(mnProduct.Factorizations) > 0 {
+      break
+    }
+  }
 
-	if len(mnProduct.Factorizations) == 0 {
-		return mnProduct, mxProduct, errors.New("no palindromes...")
-	}
+  if len(mnProduct.Factorizations) == 0 {
+    return mnProduct, mxProduct, errors.New("no palindromes...")
+  }
 
-	for mx = ffmax; mx > mn; mx-- {
-		if !isPalindrome(mx) {
-			continue
-		}
-		mxProduct = Product{mx, getFactorizations(mx, fmin, fmax)}
-		if len(mxProduct.Factorizations) > 0 {
-			break
-		}
-	}
+  for mx = ffmax; mx > mn; mx-- {
+    if !isPalindrome(mx) {
+      continue
+    }
+    mxProduct = Product{mx, getFactorizations(mx, fmin, fmax)}
+    if len(mxProduct.Factorizations) > 0 {
+      break
+    }
+  }
 
-	if len(mxProduct.Factorizations) == 0 {
-		return mnProduct, mxProduct, errors.New("no palindromes...")
-	}
+  if len(mxProduct.Factorizations) == 0 {
+    return mnProduct, mxProduct, errors.New("no palindromes...")
+  }
 
-	return mnProduct, mxProduct, nil
+  return mnProduct, mxProduct, nil
 }
